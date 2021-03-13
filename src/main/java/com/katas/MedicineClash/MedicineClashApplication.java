e6248295cb131a659621abc53b25a7ec3d2893b6
Chris Rucci <chris.rucci@gmail.com>
2021-03-13 09:57:30 -0500

    Initial Commit

---
+++ src/main/java/com/katas/MedicineClash/MedicineClashApplication.java
@@ -0,0 +1,13 @@
+package com.katas.MedicineClash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineClashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineClashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/katas/MedicineClash/MedicineClashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katas.MedicineClash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicineClashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
